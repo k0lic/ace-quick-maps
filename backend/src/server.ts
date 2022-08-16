04ddffd159f50c769b4f1ed1a8e7544f450a7d64
Andrija Kolic <kolicandrija98@gmail.com>
2022-08-16 22:12:01 +0200

    Added routes for working with tour programs, program days and points. Hope they'll be useful, since I've yet to do the frontend part.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -73,6 +73,86 @@ router.post('/delete_location', (req, res) => {
     executeQueryWithoutResults(query_string, res);
 });
 
+// Program routes
+router.get('/all_tour_programs', (req, res) => {
+    let query_string = 'SELECT * FROM programs pr, partners pa WHERE pr.idpartner = pa.idpartner';  // join with partners might be unneeded
+    executeQuery(query_string, res);
+});
+
+router.post('/tour_program_days', (req, res) => {
+    let program_id = req.body.id;
+
+    // TODO: what if there's a day without any points? that will be missed, no?
+    let query_string = ''
+        + 'SELECT * '
+        + 'FROM program_days d, points p, locations l '
+        + 'WHERE d.idprogram = \'' + program_id + '\' '
+        + '     AND p.idprogram = d.idprogram AND p.daynumber = d.number '
+        + '     AND l.name = p.location';
+    executeQuery(query_string, res);
+});
+
+router.post('/add_program_day', (req, res) => {
+    let program_id = req.body.id;
+    let number = req.body.number;
+    let description = req.body.description;
+
+    let query_string = 'INSERT INTO program_days (idprogram, number, description) VALUES (\'' + program_id + '\',\'' + number + '\',\'' + description + '\')';
+    executeQueryWithoutResults(query_string, res);
+});
+
+router.post('/delete_program_day', (req, res) => {
+    // TODO: remove all the points first, yeah?
+});
+
+router.post('/add_point', (req, res) => {
+    let program_id = req.body.id;
+    let number = req.body.number;
+    let point_index = req.body.index;
+    let location = req.body.location;
+    let type = req.body.type;
+    let description = req.body.description;
+
+    let query_string = 'INSERT INTO points (idprogram, daynumber, pointindex, location, idtype, description) '
+        + 'VALUES ('
+        + '\'' + program_id + '\','
+        + '\'' + number + '\','
+        + '\'' + point_index + '\','
+        + '\'' + location + '\','
+        + '\'' + type + '\','
+        + '\'' + description + '\')';
+    executeQueryWithoutResults(query_string, res);
+});
+
+router.post('/update_point', (req, res) => {
+    let id = req.body.id;
+    let point_index = req.body.index;
+    let location = req.body.location;
+    let type = req.body.type;
+    let description = req.body.description;
+
+    let query_string = 'UPDATE points '
+        + 'SET pointindex = \'' + point_index + '\','
+        + '     location = \'' + location + '\','
+        + '     idtype = \'' + type + '\','
+        + '     description = \'' + description + '\' '
+        + 'WHERE idpoint = \'' + id + '\'';
+    executeQueryWithoutResults(query_string, res);
+});
+
+router.post('/delete_point', (req, res) => {
+    let id = req.body.id;
+
+    let query_string = 'DELETE FROM points WHERE idpoint = \'' + id + '\'';
+    executeQueryWithoutResults(query_string, res);
+});
+
+// Point Types routes
+router.get('/all_point_types', (req, res) => {
+    let query_string = 'SELECT * FROM point_types';
+    executeQuery(query_string, res);
+});
+
 // Query helpers
 function executeQueryWithoutResults(query_string: string, res): void {
     connection.query(query_string, (err, rows, fields) => {
